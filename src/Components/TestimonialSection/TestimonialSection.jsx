ed6a40b9f6ec5ab7e5338a86916d8dd05b144e6d
Ajayi Adeoluwa <oluwajre2412@gmail.com>
2025-04-19 13:42:27 +0100

    third commit

--- src/Components/TestimonialSection/TestimonialSection.jsx
+++ src/Components/TestimonialSection/TestimonialSection.jsx
@@ -16,7 +16,7 @@ const TestimonialSection = () => {
 
             <div className='d-flex flex-column justify-content-between ms-5 me-4'>
               <p className='text-uppercase section-title-p text-primary mb-0'>Testimonials</p>
-              <div className='display-6 fw-bold' style={{maxWidth: '400px'}}><span className='text-primary'>.</span> People <span className='text-primary'>Says</span></div>
+              <div className='display-6 fw-bold' style={{maxWidth: '400px'}}><span className='text-primary'>.</span> What People <span className='text-primary'>Say</span></div>
             </div>
 
             <div className="carousel-indicators position-relative m-0 ms-md-auto mt-4 mt-sm-0">
@@ -38,7 +38,7 @@ const TestimonialSection = () => {
 
             <div className="carousel-item active">
               <div>
-                <p className='lead fw-normal'>TriniTwin Energy transformed our business with their solar installation. We’ve seen a significant reduction in energy costs and are proud to use sustainable energy. Their team was professional and efficient throughout the entire process.</p>
+                <p className='lead fw-normal'>I had been trying to install satellite TV at my office, but the building’s complex wiring made it a real challenge. Multiple technicians came and left without a solution. Then I contacted Figap—after just one visit, they resolved the issue quickly and efficiently, where others had failed. Their service was both professional and affordable.</p>
                 {/* <p className="lead fw-normal">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad dolor quod, blanditiis cum architecto vel velit aliquam facere nobis pariatur et fugiat quos inventore laborum porro quam autem eius assumenda hic? Minus optio inventore laudantium laborum fugit perspiciatis saepe voluptatum!</p> */}
                 <p className='fw-bold mt-4'> <span className='align-middle quote-line bg-primary me-1 me-sm-3'/> <span className='align-middle'>Micheal A.</span> <span className='align-middle text-primary ms-1'>Business Owner</span></p>
               </div>
@@ -46,7 +46,7 @@ const TestimonialSection = () => {
 
             <div className="carousel-item">
               <div>
-                <p className='lead fw-normal'>The CCTV system installed by TriniTwin Energy has provided us with peace of mind knowing our property is secure 24/7. The team was knowledgeable, and the installation was seamless.</p>
+                <p className='lead fw-normal'>Figap did an excellent job installing our CCTV system. Their expertise and attention to detail made the entire process seamless. Now we have 24/7 security and the peace of mind that our property is well-protected.</p>
                 {/* <p className="lead fw-normal">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad dolor quod, blanditiis cum architecto vel velit aliquam facere nobis pariatur et fugiat quos inventore laborum porro quam autem eius assumenda hic? Minus optio inventore laudantium laborum fugit perspiciatis saepe voluptatum!</p> */}
                 <p className='fw-bold mt-4'> <span className='align-middle quote-line bg-primary me-1 me-sm-3'/> <span className='align-middle'>Sarah O.</span> <span className='align-middle text-primary ms-1'>Homeowner</span></p>
               </div>
@@ -54,7 +54,7 @@ const TestimonialSection = () => {
 
             <div className="carousel-item">
               <div>
-                <p className='lead fw-normal'>TriniTwin Energy’s electrical wiring services were top-notch. They ensured everything was done safely and efficiently, and the work complied with all industry standards. Highly recommend them for both residential and commercial projects!</p>
+                <p className='lead fw-normal'>Figap’s solar installation has been a game-changer for our homes. We've significantly cut down on energy costs and are proud to be running on clean, sustainable power. Their team was professional, efficient, and a pleasure to work with from start to finish.</p>
                 {/* <p className="lead fw-normal">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad dolor quod, blanditiis cum architecto vel velit aliquam facere nobis pariatur et fugiat quos inventore laborum porro quam autem eius assumenda hic? Minus optio inventore laudantium laborum fugit perspiciatis saepe voluptatum!</p> */}
                 <p className='fw-bold mt-4'> <span className='align-middle quote-line bg-primary me-1 me-sm-3'/> <span className='align-middle'>David T.</span> <span className='align-middle text-primary ms-1'>Property Manager</span></p>
               </div>
